950d929406d301cf4061efc4258d33077cdea35d
Theo Zourzouvillys <theo@zrz.io>
2018-06-13 07:40:06 -0700

    make java 8 compatible

--- zulu/src/main/java/io/zrz/zulu/types/AbstractZField.java
+++ zulu/src/main/java/io/zrz/zulu/types/AbstractZField.java
@@ -1,10 +1,18 @@
 package io.zrz.zulu.types;
 
+import java.util.Optional;
+
 public abstract class AbstractZField implements ZField {
 
   @Override
   public String toString() {
-    return fieldType() + (constantValue().or(this::defaultValue).map(val -> " = " + val).orElse(""));
+    return fieldType() + (
+
+    Optional.ofNullable(constantValue().orElse(defaultValue().orElse(null)))
+
+        .map(val -> " = " + val).orElse("")
+
+    );
   }
 
 }
